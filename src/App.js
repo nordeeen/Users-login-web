79b046c05ea59932e644b48a83660dcb991fa51e
AhmadNurdiansah <ahmadnrdnsah@gmail.com>
2022-10-22 08:44:05 -0700

    remove useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import UserLogin from 'components/UserLogin';
 import UserLists from 'components/UserLists';
 import UserDetails from 'components/UserDetails';
